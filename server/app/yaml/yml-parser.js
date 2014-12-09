99e410ff5fec56daa6be3df172d2d16027536a59
Saltuk Alakus <saltukalakus@gmail.com>
2014-12-09 19:30:14 +0200

    Converting YML to JSON back and forth cause some text change unintentionally. Closes #30

--- server/app/yaml/yml-parser.js
+++ server/app/yaml/yml-parser.js
@@ -1,5 +1,8 @@
-yaml = require('js-yaml');
+yaml = require('js-yaml'); //used for parsing YML files
+yaml2 = require('rsa-yamljs'); //used for converting JSON back to YML.
+
 fs   = require('fs');
+var metadata_conf = {};
 
 module.exports.readYML = function(file_in) {
     var json_out = {};
@@ -8,12 +11,22 @@ module.exports.readYML = function(file_in) {
     } catch (err) {
         console.log(err);
     }
+
+    // Load the associated metadata conf file for the loaded conf file.
+    try {
+        metadata_conf = yaml.safeLoad(fs.readFileSync('./yml/' + file_in + '-conf.yml', 'utf8'));
+    }
+    catch (err) {
+        console.log(err);
+    }
+
     return json_out;
 };
 
 module.exports.updateYML = function(json_in, file_out){
     var fs = require('fs');
-    var yml_out = yaml.safeDump(json_in);
+    yml_out = yaml2.dump(json_in, null, null, null, null, metadata_conf);
+
     fs.writeFile('./yml/' + file_out + '-out.yml', '---\n' + yml_out , function(err) {
         if(err) {
             console.log(err);
