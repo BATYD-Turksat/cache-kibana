3659e9f4ff806ce20366b573cb62ee54c179b9f6
Saltuk Alakus <saltukalakus@gmail.com>
2015-05-12 11:27:40 +0300

    Disable HA for now

--- server/infra/mongodb/init.sh
+++ server/infra/mongodb/init.sh
@@ -24,9 +24,9 @@ cp -v backup.conf /data-mongodb/backup
 
 # First time configuration script, this needs to be executed once.
 mongod -f ./primary.conf
-mongod -f ./secondary.conf
-mongod -f ./backup.conf
-mongo 127.0.0.1:27001/admin init.js
+#mongod -f ./secondary.conf
+#mongod -f ./backup.conf
+#mongo 127.0.0.1:27001/admin init.js
 
 # Kill all running mongod processes
 pkill -9 mongod
