db8fdfd8639bfb860a75763755b1eec44d2a06a2
Saltuk Alakus <saltukalakus@gmail.com>
2015-06-01 16:43:14 +0300

    Enable jwt token for insecure api Closes #27

---
+++ server/app/models/tokenModel.js
@@ -0,0 +1,77 @@
+var mongoose = require('mongoose');
+var jwt = require('jwt-simple');
+var tokenConfig = require('../../config/token');
+var tokenSchema = require('./tokenSchema');
+
+var TokenModel = mongoose.model('Token', tokenSchema);
+
+TokenModel.encode = function(data) {
+    return jwt.encode(data, tokenConfig.tokenSecret);
+};
+
+TokenModel.decode = function(data) {
+    return jwt.decode(data, tokenConfig.tokenSecret);
+};
+
+TokenModel.findUserByToken = function(token, done) {
+    var decoded = this.decode(token);
+    if (decoded && decoded.email) {
+        this.findOne({'email': decoded.email}, function (err, usr) {
+            if (err || !usr) {
+                done(err, null);
+            } else if (usr.email === decoded.email) {
+                done(false, {email: usr.email});
+            } else {
+                done(new Error('Token does not exist or does not match.'), null);
+            }
+        });
+    } else {
+        done(new Error('Could not decode the token.'), null);
+    }
+};
+
+TokenModel.createToken = function(email, done) {
+    self = this;
+    this.findOne({'email': email}, function(err, user) {
+        if(err || email === "") {
+            console.log('err');
+            done(true, null);
+        }
+        if (user && user.token !==null){
+            console.log("token found!");
+            done(false, user);//token object, in turn
+        } else {
+            user = new TokenModel();
+            user.email = email;
+            user.date_created = Date.now();
+            user.token =  self.encode({'email': user.email, 'date_created': user.date_created });
+
+            user.save(function(err) {
+                if (err) {
+                    done(err, null);
+                } else {
+                    done(false, user);//token object, in turn
+                }
+            });
+        }
+    });
+};
+
+TokenModel.invalidateToken = function(email, done) {
+    this.findOne({'email': email}, function(err, usr) {
+        if(err || !usr) {
+            console.log('err');
+        }
+        usr.token = null;
+        usr.save(function(err, usr) {
+            if (err) {
+                done(err, null);
+            } else {
+                done(false, 'removed');
+            }
+        });
+    });
+};
+
+// create the model for users and expose it to our app
+module.exports = TokenModel;
--- server/app/routes.js
+++ server/app/routes.js
@@ -1,5 +1,6 @@
 module.exports = function(app, passport) {
 var User       = require('./models/userModel');
+var Token      = require('./models/tokenModel');
 
 // normal routes ===============================================================
 
@@ -7,6 +8,14 @@ var User       = require('./models/userModel');
 	app.get('/', function(req, res) {
         if (req.isAuthenticated()) {
             var user = req.user;
+
+            // Create web token
+            Token.createToken(user.local.email, function(err, token){
+                if (err) {
+                    console.log(err);
+                }
+            });
+
             res.cookie('user-name', user.local.email, { maxAge: 2592000000 });
 
             if (user.local.role == 'admin'){
@@ -26,6 +35,11 @@ var User       = require('./models/userModel');
 
 	// LOGOUT ==============================
 	app.get('/logout', function(req, res) {
+        Token.invalidateToken(req.user.local.email, function(err, token){
+            if (err) {
+                console.log(err);
+            }
+        });
 		req.logout();
 		res.redirect('/');
 	});
@@ -200,5 +214,67 @@ var User       = require('./models/userModel');
         } else {
             res.redirect('/');
         }
+    });
+
+    // =============================================================================
+    // Token API                                                       =============
+    // =============================================================================
+
+    // Authenticates with the session cookie. This api should be called after login
+    // in a web applications.
+    app.get('/api/token/generate', function(req, res) {
+        if (req.isAuthenticated()) {
+            console.log("This is the e-mail: " + req.user.local.email);
+            Token.createToken(req.user.local.email, function (err, token) {
+                if (err) {
+                    console.log(err);
+                    res.json({error: err});
+                } else {
+                    res.json({
+                        email: token.email,
+                        token: token.token
+                    });
+                }
+            });
+        } else {
+            res.json({error: "You need to login first"});
+        }
+    });
+
+    app.get('/api/token/invalidate', function(req, res) {
+        if (req.isAuthenticated()) {
+            Token.invalidateToken(req.user.local.email, function (err, token) {
+                if (err) {
+                    console.log(err);
+                    res.json({error: err});
+                } else {
+                    res.json({
+                        email: token.email,
+                        token: token.token
+                    });
+                }
+            });
+        } else {
+            res.json({error: "You need to login first"});
+        }
+    });
+
+    app.get('/api/token/test', function(req, res) {
+        console.log(req);
+        var incomingToken = req.headers.token;
+        console.log('incomingToken: ' + incomingToken);
+        Token.findUserByToken(incomingToken, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.json({error: err});
+            } else {
+                res.json({
+                    log: {
+                        user: user,
+                        message: "This is just a simulation of an API endpoint"
+                    }
+                });
+            }
+        });
     });
 };
